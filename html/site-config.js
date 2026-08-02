add89f83a66b0eddbf620a39c1ea3eea67a48ba6
en0000 <t.endo.system@gmail.com>
2026-08-02 12:31:05 +0900

    Publish download link for v1.0.0

--- html/site-config.js
+++ html/site-config.js
@@ -1,7 +1,7 @@
-// ダウンロードを公開するときだけパスを設定してください。
+﻿// ダウンロードを公開するときだけパスを設定してください。
 // 例: downloadPath: "downloads/SqlServerLiveMover.zip"
 // 空文字のままなら、ダウンロードリンクとセクションは表示されません。
 window.SqlMoverSiteConfig = {
-  downloadPath: "",
-  version: ""
+  downloadPath: "https://github.com/Bazzgard/SqlServerLiveMover/releases/download/v1.0.0/SqlServerLiveMover-v1.0.0-win-x64.zip",
+  version: "1"
 };
